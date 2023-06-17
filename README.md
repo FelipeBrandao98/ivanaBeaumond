7c7fea7a750a1994dd0a13e160a04e542146e5eb
Felipe Brandão <felipe.brandao98@icloud.com>
2023-06-17 13:26:35 -0300

    06-17-2023 - 2

--- README.md
+++ README.md
@@ -164,3 +164,5 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/deploym
 ## 06-17-2023
 
 ### 1 - Adição do BlogContentTwo
+
+### 2 - Adicionado a descrição da imagem no BlogContentOne
--- src/components/PTBR/BLOG/BlogContentBR/BlogContentOneBR/index.jsx
+++ src/components/PTBR/BLOG/BlogContentBR/BlogContentOneBR/index.jsx
@@ -6,13 +6,16 @@ export default function BlogContentOneBR() {
   return (
     <div className={styles.contentOne}>
       <p className={styles.contentOneText}><span className={styles.contentOneTextFirstLetter}>R</span>ealizado na Fazenda São Jerônimo, Ilha do Marajó-PA, lugar que foi palco da gravação de diversos realitys de sucesso, incluindo o “No Limite” da Rede Globo, Ivana explica sobre o porquê da escolha desse local e a relação entre a natureza e vestidos de noiva.</p>
-      <Image
-        src='/photos_blog/photo_blog_1.jpeg'
-        alt='Foto Ivana Beaumond'
-        width={1920}
-        height={1080}
-        className={styles.contentOneImage}
-      />
+      <div className={styles.contentTwoImageArea}>
+        <Image
+          src='/photos_blog/photo_blog_1.jpeg'
+          alt='Foto Ivana Beaumond'
+          width={1920}
+          height={1080}
+          className={styles.contentOneImage}
+        />
+        <p className={styles.contentTwoImageDescriptionOne}>Soluta quisquam aliquid, dolor harum unde! Nihil.</p>
+      </div>
     </div>
   )
 }
