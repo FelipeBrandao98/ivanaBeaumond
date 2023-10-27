0d86a1ccce9d69a038741cbb307ffb06b2c8e6dd
Felipe Brandão <felipe.brandao98@icloud.com>
2023-10-27 14:28:14 -0300

    comments in files on @/components/IB-MAILER folder, estructured code

--- src/components/IB-MAILER/MailerPage/index.jsx
+++ src/components/IB-MAILER/MailerPage/index.jsx
@@ -1,13 +1,18 @@
 'use client'
 
+// Context imports
 import { AuthContext } from '@/Context/AuthContext'
 import { useContext } from 'react'
 
+// Styles imports
 import styles from './styles.module.css'
 
+// Component Declaration
 export default function MailerPage({ children }) {
+  // Instanciate and initialize Contexts functions
   const { token } = useContext(AuthContext)
 
+  // Return components, with functions to call API and language
   return (
     <>
       <h1>Mailer</h1>
@@ -24,4 +29,5 @@ export default function MailerPage({ children }) {
       </section>
     </>
   )
+  //
 }
