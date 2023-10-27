6e95661ab849f5d79594da90a653ced4bb0d16fe
Felipe Brandão <felipe.brandao98@icloud.com>
2023-10-27 14:46:22 -0300

    comments in files on @/Hooks folder, estructured code

--- src/hooks/CheckIfIsLogged/index.jsx
+++ src/hooks/CheckIfIsLogged/index.jsx
@@ -1,14 +1,23 @@
 'use client'
 
-import { AuthContext } from '@/Context/AuthContext'
-import AccessUnauthorized from '@/components/LOGIN/AccessUnauthorized'
-import Dashboard from '@/components/LOGIN/Dashboard'
+// React imports
 import { useContext } from 'react'
 
+// Context imports
+import { AuthContext } from '@/Context/AuthContext'
+
+// Componentsimports
+import AccessUnauthorized from '@/components/LOGIN/AccessUnauthorized'
+import Dashboard from '@/components/LOGIN/Dashboard'
+
+// Component Declaration
 export default function CheckIfIsLogged({ children }) {
+  // Instanciate and initialize Contexts functions
   const { token } = useContext(AuthContext)
 
+  // Return Dashboard or block if has no token
   return (
     <>{token ? <Dashboard>{children}</Dashboard> : <AccessUnauthorized />}</>
   )
+  //
 }
