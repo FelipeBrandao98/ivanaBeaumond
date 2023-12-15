df3583044dbc487b1332efba66065a84d879937d
Felipe Brandão <felipe.brandao98@icloud.com>
2023-12-15 12:16:40 -0300

    finalized collections page

--- src/components/EndUsersRoute/PAGES/COLLECTIONS/MainPageCollections/Collections/index.jsx
+++ src/components/EndUsersRoute/PAGES/COLLECTIONS/MainPageCollections/Collections/index.jsx
@@ -13,24 +13,14 @@ export default function Collections({ lang, data }) {
     <section className={styles.section}>
       {data.map((data) => {
         return (
-          <>
-            <Collection
-              key={data.id}
-              collectionId={data.id}
-              lang={lang}
-              title={data.title}
-              description={data.description}
-              imageUrl={data.cover.url || ''}
-            />
-            <Collection
-              key={data.id + 1}
-              collectionId={data.id}
-              lang={lang}
-              title={data.title}
-              description={data.description}
-              imageUrl={data.cover.url || ''}
-            />
-          </>
+          <Collection
+            key={data.id}
+            collectionId={data.id}
+            lang={lang}
+            title={data.title}
+            description={data.description}
+            imageUrl={data.cover.url || ''}
+          />
         )
       })}
     </section>
