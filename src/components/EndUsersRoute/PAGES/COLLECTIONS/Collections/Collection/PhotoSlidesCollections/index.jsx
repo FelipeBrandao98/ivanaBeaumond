18abcc59985739fbad4ee2cd04b2007c24b7f02d
Felipe Brandão <felipe.brandao98@icloud.com>
2023-12-29 15:58:19 -0300

    add modal to show image in collections

--- src/components/EndUsersRoute/PAGES/COLLECTIONS/Collections/Collection/PhotoSlidesCollections/index.jsx
+++ src/components/EndUsersRoute/PAGES/COLLECTIONS/Collections/Collection/PhotoSlidesCollections/index.jsx
@@ -11,12 +11,15 @@ import { FiArrowLeft, FiArrowRight } from 'react-icons/fi'
 
 // Styles imports
 import styles from './styles.module.css'
+import { MdClose } from 'react-icons/md'
 
 // Component Declaration
 export default function PhotoSlidesCollections({ images }) {
   // States declaratios
   const [slide, setSlide] = useState(2)
   const [slideClass, setSlideClass] = useState(styles.secondSlide)
+  const [imageShowModal, setImageShowModal] = useState(false)
+  const [imageInModal, setImageInModal] = useState('')
 
   // Functions to manipulate window object
   function changeSlideFromRight() {
@@ -68,49 +71,76 @@ export default function PhotoSlidesCollections({ images }) {
 
   // Return components, with functions to call API and language
   return (
-    <div className={styles.container}>
-      <div className={styles.fromLeftArea}>
-        <button className={styles.fromLeftButton} onClick={changeSlideFromLeft}>
-          <FiArrowLeft
-            className={styles.fromLeftImage}
-            width={100}
-            height={100}
-          />
-        </button>
-      </div>
+    <>
+      <div className={styles.container}>
+        <div className={styles.fromLeftArea}>
+          <button
+            className={styles.fromLeftButton}
+            onClick={changeSlideFromLeft}
+          >
+            <FiArrowLeft
+              className={styles.fromLeftImage}
+              width={100}
+              height={100}
+            />
+          </button>
+        </div>
 
-      <div
-        className={`
+        <div
+          className={`
         ${styles.SlideArea}
         ${slideClass}
         `}
-      >
-        {images
-          ? images.map((image) => {
-              return (
-                <Image
-                  key={image.id}
-                  className={styles.image}
-                  src={image.cover.url}
-                  alt={image.collection.title}
-                  width={1620}
-                  height={1280}
-                />
-              )
-            })
-          : ''}
-      </div>
+        >
+          {images
+            ? images.map((image) => {
+                return (
+                  <>
+                    <Image
+                      key={image.id}
+                      className={styles.image}
+                      src={image.cover.url}
+                      alt={image.collection.title}
+                      width={1620}
+                      height={1280}
+                      onClick={(e) => {
+                        setImageShowModal(true)
+                        setImageInModal(image.cover.url)
+                      }}
+                    />
+                    {imageShowModal && (
+                      <div className={styles.modal}>
+                        <MdClose
+                          width={50}
+                          height={50}
+                          onClick={(e) => setImageShowModal(false)}
+                        />
+                        <Image
+                          className={styles.imageInModal}
+                          src={imageInModal}
+                          alt="Ivana Beaumond"
+                          width={1620}
+                          height={1280}
+                        />
+                      </div>
+                    )}
+                  </>
+                )
+              })
+            : ''}
+        </div>
 
-      <div className={styles.fromRightArea} onClick={changeSlideFromRight}>
-        <button className={styles.fromRightButton}>
-          <FiArrowRight
-            className={styles.fromRightImage}
-            width={100}
-            height={100}
-          />
-        </button>
+        <div className={styles.fromRightArea} onClick={changeSlideFromRight}>
+          <button className={styles.fromRightButton}>
+            <FiArrowRight
+              className={styles.fromRightImage}
+              width={100}
+              height={100}
+            />
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   )
   //
 }
